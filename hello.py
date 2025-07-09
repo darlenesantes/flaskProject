b31fab711bd711250f5ac3372ea97cfb33d70e58
Darlene Santes <darlenesantes@gmail.com>
2025-07-09 16:40:47 +0000

    initial commit

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+
+app.config['SECRET_KEY'] = '06c2e0da2228af49ebeae9b1ceadde75'
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', subtitle='Home Page', text='This is Dar\'s Dash!')
+    
+@app.route("/second_page")
+def second_page():
+    return render_template('second_page.html', subtitle='About me!', text='Here are some interesting facts about me.')
+    
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        username = form.username.data
+        return redirect(url_for('third_page', username=username)) # if so - send to secret third page
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/secret_gallery/<username>")
+def third_page(username):
+  return render_template('third_page.html', subtitle='Secret Gallery??', text='These are some of my fav pictures! Just so you can get to know me a little more.', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
