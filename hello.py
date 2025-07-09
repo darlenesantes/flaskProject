a436b0d81d3314b59740b7ba0ac3161dd5efcd2c
Darlene Santes <darlenesantes@gmail.com>
2025-07-09 12:43:02 -0500

    Add webhook route for server updates and import request module

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 app = Flask(__name__)
@@ -25,6 +26,19 @@ def register():
 @app.route("/secret_gallery/<username>")
 def third_page(username):
   return render_template('third_page.html', subtitle='Secret Gallery??', text='These are some of my fav pictures! Just so you can get to know me a little more.', username=username)
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
